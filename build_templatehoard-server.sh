d638f7af47d6c4f462d6d920e600060dc3c112ae
Erutuon <5840197+Erutuon@users.noreply.github.com>
2020-01-29 10:15:05 +0000

    add server

---
+++ build_templatehoard-server.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+cd ~/git/templatehoard-server
+RUSTFLAGS='--emit=asm -C target-cpu=native' ~/.cargo/bin/cargo build --release
---
+++ server.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+cd ~/git/templatehoard-server
+~/bin/server templatehoard
