6b2a27579a6356a3d80b26a9a7e167efc1f263ec
Erutuon <5840197+Erutuon@users.noreply.github.com>
2020-04-13 21:52:01 +0000

    retrieve template names from a dedicated page too

--- gather_template_names.py
+++ gather_template_names.py
@@ -2,12 +2,20 @@
 
 from pywikibot import Site, Page
 import re
+from traceback import print_exc
 
 site = Site(code="en", fam="wiktionary")
-title = "Wiktionary:Templates with current language parameter"
-page = Page(site, title=title)
+def gather_template_names(title):
+    page = Page(site, title=title)
+    return (match.group(1) for match in re.finditer(r"\{\{temp\|([^\}]+)\}\}", page.text))
 
-template_names = [ match.group(1) for match in re.finditer(r"\{\{temp\|([^\}]+)\}\}", page.text) ]
-template_names.sort(key=lambda name: (name.lower(), name))
-print("\n".join(template_names))
+try:
+    template_names = set(gather_template_names("User:Erutuon/lists/templatehoard templates"))
+    template_names.update(gather_template_names("Wiktionary:Templates with current language parameter"))
 
+    template_names = list(template_names)
+    template_names.sort(key=lambda name: (name.lower(), name))
+
+    print("\n".join(template_names))
+except Exception as e:
+    print_exc()
