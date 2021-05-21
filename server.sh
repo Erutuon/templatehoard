5b049b1e68434803c1966fb59f7bd9973f15e3c6
Erutuon <5840197+Erutuon@users.noreply.github.com>
2021-05-21 19:26:13 +0000

    remove server.sh

--- server.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-DUMP_DATE=20201120
-RUST_LOG=all ~/bin/server --cbor ~/www/static/dump/$DUMP_DATE --redirects ~/template_redirects/$DUMP_DATE.json --static ~/git/templatehoard-server/static --port $PORT >> ~/logs/server.out 2>> ~/logs/server.err
