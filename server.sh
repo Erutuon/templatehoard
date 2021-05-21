9de40e1bae9cefc3f6436717acb7043822d2cfcb
Erutuon <5840197+Erutuon@users.noreply.github.com>
2021-05-21 19:25:47 +0000

    last version of server.sh (not used anymore)

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-cd ~/git/templatehoard-server
-RUST_LOG=all ~/bin/server templatehoard >> ~/logs/server.out 2>> ~/logs/server.err
+DUMP_DATE=20201120
+RUST_LOG=all ~/bin/server --cbor ~/www/static/dump/$DUMP_DATE --redirects ~/template_redirects/$DUMP_DATE.json --static ~/git/templatehoard-server/static --port $PORT >> ~/logs/server.out 2>> ~/logs/server.err
