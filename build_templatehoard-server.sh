5ac40f42006f943558ba48fa17b32fd4da6890af
Erutuon <5840197+Erutuon@users.noreply.github.com>
2021-05-21 19:27:24 +0000

    target-cpu=native causes problems on Grid Engine

--- build_templatehoard-server.sh
+++ build_templatehoard-server.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 cd ~/git/templatehoard-server
-RUSTFLAGS='--emit=asm -C target-cpu=native' ~/.cargo/bin/cargo build --release && webservice restart
+RUSTFLAGS='--emit=asm' ~/.cargo/bin/cargo build --release
